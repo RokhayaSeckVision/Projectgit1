a797f8c7b8bc2dbf7cb01bba34dcec8841b800e2
RokhayaSeckVision <143196141+RokhayaSeckVision@users.noreply.github.com>
2023-11-22 15:22:56 +0000

    Add files via upload

---
+++ somme2.php
@@ -0,0 +1,26 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Somme</title>
+    <link rel="stylesheet" href="Style.css">
+    <script src="https://kit.fontawesome.com/6cb35a5004.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <h2>
+        <?php
+            $number=5; 
+            $number2=10;
+            $sum=$number + $number2;
+            echo "La somme de $number et de $number2 est de $sum"
+
+
+        ?>
+    </h2>
+
+</body>
+
+</html>
